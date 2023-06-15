d9a0d0c62232cfc367468eeaded6f2481b70af5e
aivanov <alex130896@gmail.com>
2023-06-15 17:16:57 +0400

    Add background Image
    Добавил фоновое изображение

--- iOS10-HW11-Alexandr-Ivanov/Sources/ViewController.swift
+++ iOS10-HW11-Alexandr-Ivanov/Sources/ViewController.swift
@@ -9,7 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - UI Elements
+
+    private lazy var backgroundImage: UIImageView = {
+        let image = UIImage(named: "backgroundImage")
+        let imageView = UIImageView(image: image)
+        imageView.contentMode = .scaleAspectFill
+
+        return imageView
+    }()
+
+    // MARK: - Life Cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupHierarchy()
+    }
+
+    // MARK: - Setups
+
+    private func setupHierarchy() {
+        view.addSubview(backgroundImage)
     }
 }
